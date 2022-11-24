68d7095928be2180f7c3e982f05467277164d153
Hai Truong DFI <hai.truong@datafluct.com>
2022-11-24 23:01:28 +0700

    commit code for DSG

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,12 +7,44 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  // result from graphql is a object. We just mention to property "data"
+  const { data, errors } = await graphql(
+    `
+    {
+      allContentfulBlogPost(filter: {node_locale: {eq: "en-US"}}) {
+        nodes {
+          title
+          contentful_id
+          body {
+            raw
+          }
+          publishDate
+          slug
+        }
+      }
+    }
+    `
+  );
+
+  if (errors) {
+    reporter.panicOnBuild(`Error while running GraphQL Query`);
+    return;
+  }
+
+  const { createPage } = actions // destructuring "actions" object to take care just createPage action (a property)
+
+  data.allContentfulBlogPost.nodes.forEach((item) => { // scan all items of blog post to create page (item)
+
+    createPage({ // with each item, creating and routing to a page
+      path: item.slug, //path of deferred page
+      component: require.resolve("./src/templates/using-dsg.js"), // template DSG page to fill data
+      context: { post: item, $post: item }, // pass a context page property for each page
+      // deffer property is marked to deffered page
+      defer: item.slug !== "hello-world", // just wanting up-front 'hello-world' page. The rest pages will be deferred
+    })
+
+  });
+
 }
--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -1,26 +1,16 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
+import { renderRichText } from "gatsby-source-contentful/rich-text";
 import Layout from "../components/layout"
-import Seo from "../components/seo"
 
-const UsingDSG = () => (
-  <Layout>
-    <h1>
-      Hello from a <b>DSG Page</b>
-    </h1>
-    <p>This page is not created until requested by a user.</p>
-    <p>
-      To learn more, head over to our{" "}
-      <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/deferred-static-generation/">
-        documentation about Deferred Static Generation
-      </a>
-      .
-    </p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const UsingDSG = (props) => {
+  console.log(props);
+  return (
+    <Layout>
+      <h1>{props.pageContext.post.title}</h1>
+      <p>{renderRichText(props.pageContext.post.body)}</p>
+    </Layout>
+  );
 
-export const Head = () => <Seo title="Using DSG" />
+}
 
-export default UsingDSG
+export default UsingDSG;
