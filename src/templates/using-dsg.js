88a95e9c2e2c369a2129c37af6e93c91a5d6eb5e
Hai Truong DFI <hai.truong@datafluct.com>
2022-11-25 10:14:10 +0700

    add props to Head API in using-dsg.js file

--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -1,16 +1,17 @@
 import * as React from "react"
 import { renderRichText } from "gatsby-source-contentful/rich-text";
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const UsingDSG = (props) => {
-  console.log(props);
+  //console.log(props);
   return (
     <Layout>
       <h1>{props.pageContext.post.title}</h1>
-      <p>{renderRichText(props.pageContext.post.body)}</p>
+      {renderRichText(props.pageContext.post.body)}
     </Layout>
   );
 
 }
-
+export const Head = (props) => <Seo title={props.pageContext.post.title} />
 export default UsingDSG;
